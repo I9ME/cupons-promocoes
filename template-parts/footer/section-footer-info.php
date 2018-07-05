5c280d461a8bde320cc13bec3749f54ba41bd3b5
DIOGO SOARES <diogo@i9me.com.br>
2018-07-05 09:16:12 -0300

    Arquivos adicionados via http

---
+++ template-parts/footer/section-footer-info.php
@@ -0,0 +1,43 @@
+
+<section class="Section Section--style2 Section--footerInfo Section--style2 u-paddingVertical u-paddingHorizontal--inter">
+
+	<div class="Section-container u-displayFlex u-sizeFull u-positionRelative u-marginBottom">
+		<ul class="SocialMediaBar u-positionRelative u-displayFlex u-flexDirectionRow u-sizeFull u-flexJustifyContentCenter">
+			<li class="SocialMediaBar-site u-displayBlock u-marginRight--inter">
+				<a class="FigureIcon FigureIcon--socialMedia--site u-displayBlock" href="#"></a>
+			</li>
+			<li class="SocialMediaBar-facebook u-displayBlock u-marginRight--inter">
+				<a class="FigureIcon FigureIcon--socialMedia--facebook u-displayBlock" href="#"></a>
+			</li>
+			<li class="SocialMediaBar-instagram u-displayBlock">
+				<a class="FigureIcon FigureIcon--socialMedia--instagram u-displayBlock" href="#"></a>
+			</li>
+			<li class="SocialMediaBar-youtube u-displayBlock">
+				<a class="FigureIcon FigureIcon--socialMedia--youtube u-displayBlock" href="#"></a>
+			</li>
+		</ul>
+	</div>
+
+
+	<div class="u-maxSize--container u-alignCenterBox u-displayFlex u-flexDirectionColumn u-flexSwitchRow">
+
+
+		<div class="Section-subSection Section-subSection--bio u-size8of24 u-marginBottom--inter--half">
+			<p class="u-alignCenter">
+				© Welton Lemos 2018.<br/>
+				Todos os direitos Resevados.
+			</p>
+		</div>
+		<div class="Section-subSection Section-subSection--contact u-size10of24 u-marginBottom--inter u-displayFlex u-flexJustifyContentCenter">
+			<p class="u-displayFlex u-flexDirectionRow u-flexAlignItemsCenter u-flexJustifyContentCenter">
+				<i><svg class="SocialBar-item-icon u-icon iconEnvelope u-displayInlineBlock">
+						<use xlink:href="#iconEnvelope"></use>
+					</svg>
+				</i>
+				<span>contato@weltonlemos.com.br</span></p>
+		</div>
+		<div class="Section-subSection Section-subSection--author u-size6of24 u-alignCenter">
+			<a href="https://www.i9me.com.br" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-i9me-web-design.png" title="I9ME WEB & DESIGN" alt="I9ME WEB & DESIGN" /></a>
+		</div>
+	</div> <!-- Max Size Container -->
+</section>
---
+++ template-parts/page/intro-page.php
@@ -0,0 +1,17 @@
+<section id="up" class="Section--intro Intro Intro--home Section--style2 u-positionRelative u-flex u-flexDirectionColumn u-flexAlignItemsCenter u-flexJustifyContentCenter u-paddingVertical" data-paroller-factor="-0.20" data-paroller-factor-xs="0.2">
+
+
+	<div class="Captions u-displayFlex u-flexJustifyContentCenter">		
+		<h2 class="Captions--title">
+			<span class="u-isInvisible">Cupons e Promoções Barney's Burger</span>
+			
+			<!-- <img class="u-displayBlock u-sizeAuto u-sizeHeightAuto u-maxSize100" src="<?php echo get_template_directory_uri() ?>/assets/images/caption-cupons-e-promocoes-barneys.png" alt="Cupons e Promoções Barney's Burger"> -->
+
+			 <img class="u-displayBlock u-sizeAuto u-sizeHeightAuto u-maxSize100" src="<?php echo get_template_directory_uri() ?>/assets/images/caption-cupons-e-promocoes-barneys.png" alt="Cupons e Promoções Barney's Burger" data-paroller-factor="0.35" data-paroller-factor-xs="0.2" /> 
+
+		</h2>
+		
+	</div>
+
+</section>
+
